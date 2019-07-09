fab9916c7917ee76174287854c352d55b51752f4
fadwa <fadwaana99@gmail.com>
2019-07-09 17:48:02 +0200

    second commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,7 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
-
+use App\Article;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
@@ -23,5 +23,13 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body'  => $faker->paragraph,
     ];
 });
